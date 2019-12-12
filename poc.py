8851f5d7efc8bf2eea4fcd97e59fe11ad3fe9e76
Christian Young <christian_a_young@icloud.com>
2019-12-12 23:00:58 +1300

    Update poc.py

--- poc.py
+++ poc.py
@@ -3,15 +3,15 @@ import pandas as pd
 import re
 
 #LOAD FILES
-sql_load = pd.read_csv("C:\\Users\\CHRIS\\Documents\\Development\\sql_tester\\ddl.sql",sep='\n',header=None, squeeze=1)
-data_types_redshift = pd.read_csv("C:\\Users\\CHRIS\\Documents\\Development\\sql_tester\\data_types_redshift.csv",sep='\n',header=None)
+sql_load = pd.read_csv("C:\\Users\\CHRIS\\Documents\\Development\\SQL_Test_Script_Gen\\ddl.sql",sep='\n',header=None, squeeze=1)
+data_types_redshift = pd.read_csv("C:\\Users\\CHRIS\\Documents\\Development\\SQL_Test_Script_Gen\\data_types_redshift.csv",sep='\n',header=None)
 
 
 #GET TABLE NAME
 sql_header = sql_load.iloc[0]
 sql_table =  re.search('\S+\.\S+',sql_header, flags=re.U)[0]
 
-#
+#READ THE COL NAMES AND DATA TYPES
 sql_load = pd.Series(sql_load)                                          #turn the SQL into a Series so we can conver to upper case
 sql_load = sql_load.str.upper()                                         #convert all values to upper case
 sql_load = pd.DataFrame(sql_load)                                       #convert the series into a Data Frame
@@ -25,4 +25,18 @@ sql_cols = sql_cols.loc[:,0:1]                                          #reducin
 sql_cols = sql_cols.rename(columns = {0:'COL_NAME',1:'DATA_TYPE'})      #rename columns to col_name and data_type 
 #print (sql_cols)
 
+def col_func(a,b,c):
+    if      b == 'BIGINT': 
+        print("select min(",a,"), avg(",a,"), max(",a,") from",c,";" )
+    elif    b == 'TIMESTAMP': 
+        print("select min(",a,"), max(",a,") from",c,";" )
+    elif    b == 'VARCHAR(36)': 
+        print("select",a,", count(*) from",c,"group by 1 order by 2 desc limit 50;" )
+        print("select count(distinct(",a,")), count(*) from",c,"group by 1 order by 2 desc limit 50;" )
+    elif    b == 'BOOL': 
+        print("select",a,", count(*) from",c,"group by 1 order by 1;" )
 
+for index, row in sql_cols.iterrows():
+    col_func(row['COL_NAME'], row['DATA_TYPE'], sql_table)
+
+    
