280e7f3ab8f47c40cd64e530fa9eded13d04cef7
Christian Young <12378656+ronsoak@users.noreply.github.com>
2019-12-17 21:12:39 +1300

    More stuff was done

--- poc.py
+++ poc.py
@@ -3,14 +3,20 @@ import pandas as pd
 import re
 
 #LOAD FILES
-sql_load = pd.read_csv("C:\\Users\\CHRIS\\Documents\\Development\\SQL_Test_Script_Gen\\ddl.sql",sep='\n',header=None, squeeze=1)
+#sql_load = pd.read_csv("C:\\Users\\CHRIS\\Documents\\Development\\SQL_Test_Script_Gen\\ddl.sql",sep='\n',header=None, squeeze=1)
+sql_load = pd.read_clipboard(sep='\n',header=None, squeeze=1)
 
 #GET TABLE NAME
 sql_header = sql_load.iloc[0]
 sql_table =  re.search('\S+\.\S+',sql_header, flags=re.U)[0]
 
+#FILE OUTPUT
+sql_table_file = sql_table.replace(".","_")
+filename = sql_table_file+"_testing_script.txt"
+sql_output = open(filename,"w",newline='\n')
+
 #REDSHIFT DATA TYPES
-red_nums    = ("SMALLINT","INT2","INTEGER","INT","INT4","BIGINT","INT8","DECIMAL","NUMERIC","REAL","FLOAT4","DOUBLE PRECISION","FLOAT8","FLOAT")
+red_nums    = ("SMALLINT","INT2","INTEGER","INT","INT4","BIGINT","INT8","DECIMAL","NUMERIC","REAL","FLOAT4","DOUBLE","DOUBLE PRECISION","FLOAT8","FLOAT")
 red_bool    = ("BOOL","BOOLEAN")
 red_dates   = ("DATE","TIMESTAMP","TIMESTAMPTZ")
 red_string  = ("CHAR","CHARACTER","NCHAR","BPCHAR","VARCHAR","CHARACTER VARYING","NVARCHAR","TEXT")
@@ -27,18 +33,35 @@ sql_cols = sql_load.iloc[sql_load_start+1:sql_load_end]                 #limit t
 sql_cols.reset_index(drop=1, inplace=True)                              #resets the index
 sql_cols = sql_cols['INPUT'].str.split(expand=True)                     #splitting the columns off
 sql_cols = sql_cols.loc[:,0:1]                                          #reducing just down to cols 1 & 2, dropping 3&4
-sql_cols = sql_cols.rename(columns = {0:'COL_NAME',1:'DATA_TYPE'})      #rename columns to col_name and data_type 
+sql_cols = sql_cols.rename(columns = {0:'COL_NAME',1:'DATA_TYPE'})      #rename columns to col_name and data_type
+sql_cols['DATA_TYPE'] = sql_cols['DATA_TYPE'].str.extract('(^\w*)')     #this will strip off bracketed numbers at the end varchar(45)->varchar
 
+#DEFINE FUNCTION FOR PRINTING 
 def col_func(a,b,c):
     if      b in red_nums: 
-        print("select min(",a,"), avg(",a,"), max(",a,") from",c,";" )
+        sql_output.write("select min("+a+"), avg("+a+"), max("+a+") from "+c+"; \n" )
+        
     elif    b in red_nums:
-        print("select",a,", count(*) from",c,"group by 1 order by 1;" )
+        sql_output.write("select "+a+", count(*) from "+c+" group by 1 order by 1; \n" )
+        
     elif    b in red_dates:
-        print("select min(",a,"), max(",a,") from",c,";" )
+        sql_output.write("select min("+a+"), max("+a+") from "+c+"; \n" )
+        
     elif    b in red_string:
-        print("select",a,", count(*) from",c,"group by 1 order by 2 desc limit 50;" )
-        print("select count(distinct(",a,")), count(*) from",c,"group by 1 order by 2 desc limit 50;" )
+        sql_output.write("select "+a+", count(*) from "+c+" group by 1 order by 2 desc limit 50; \n" )
+        sql_output.write("select count(distinct("+a+")), count(*) from "+c+" group by 1 order by 2 desc limit 50; \n" )
+    
+    elif    b in red_bool:
+        sql_output.write("select "+a+", count(*) from "+c+" group by 1 order by 2 desc limit 10; \n" )
+        
+    else:
+        sql_output.write("Column:"+a+"is not a know Datatype. Datatype passed was:"+b+"\n")
+        
     
 for index, row in sql_cols.iterrows():
     col_func(row['COL_NAME'], row['DATA_TYPE'], sql_table)
+
+#close file
+sql_output.close
+#End Message
+print(" Script Complete")
