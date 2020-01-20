e4addce0b9bc9c5254147e7004ee3193516b7ddf
Christian Young <12378656+ronsoak@users.noreply.github.com>
2020-01-20 19:39:14 +1300

    Added some photos for the readme and expanded the ddl

--- test_ddl.sql
+++ test_ddl.sql
@@ -1,8 +1,19 @@
-create table adhoc.test_593843_customers(
-row_id bigint   encode  AZ64,
-startdateutc timestamp  encode  AZ64,
-customer_name varchar(36)  encode  ZSTD,
-paying_flag     bool encode  ZSTD)
-diststyle       even 
+create table public.customer_table
+(
+    cust_id         bigint          encode  AZ64,
+    startdateutc    timestamp       encode  AZ64,
+    first_name      varchar(99)     encode  ZSTD,
+    last_name       varchar(99)     encode  ZSTD,
+    date_of_birth   date            encode  AZ64,
+    address_line_1  varchar(300)    encode  ZSTD,
+    address_line_2  varchar(300)    encode  ZSTD,
+    address_line_3  varchar(300)    encode  ZSTD,
+    region          varchar(300)    encode  ZSTD,
+    city            varchar(300)    encode  ZSTD,
+    postcode        int             encode  AZ64,
+    paying_flag     bool            encode  ZSTD
+)
+diststyle       key
+distkey         (cust_id)   
 sortkey         (row_id,startdateutc)
 ;
