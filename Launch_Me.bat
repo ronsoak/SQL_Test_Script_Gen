1cd98463c391960e677ed55f4e91bedab0dd638e
Ron Soak <12378656+ronsoak@users.noreply.github.com>
2020-03-21 01:25:39 +1300

    Updated CMD file
    Changes input method to be text file prompted by CMD

--- Launch_Me.bat
+++ Launch_Me.bat
@@ -1,10 +1,11 @@
 ECHO OFF
 COLOR de
-REM This script exists to promt the user to copy the DDL and to launch the script.
-ECHO Using CTRL+C Copy JUST your table create script, don't include any other SQL.
-ECHO This is only for FULL table build statements, will not work on 'Create Table as....' 
-ECHO i.e you need to have defined your columns AND DATA TYPES
-ECHO Once you have copied your table script, press Enter.
-PAUSE
+REM This script exists to promt the user to copy the DDL into the text file
+ECHO A blank text file will open, copy your DDL into this file, save and close it and hit enter in his window
+ECHO > test_script_input.sql
+test_script_input.sql
+ECHO Waiting for you to save and close file
+Echo Press Enter when ready
+PAUSE 
 ECHO Generating Test Script.... 
 C:/<path to your python install>/python.exe c:/<path to this script>/Gen_Test_Script.py
---
+++ example_script.sql
@@ -0,0 +1,19 @@
+create table public.customer_table
+(
+    cust_id         bigint          encode  AZ64,
+    startdateutc    timestamp       encode  AZ64,
+    first_name      varchar(99)     encode  ZSTD,
+    last_name       varchar(99)     encode  ZSTD,
+    date_of_birth   date            encode  AZ64,
+    address_line_1  varchar(300)    encode  ZSTD,
+    address_line_2  varchar(300)    encode  ZSTD,
+    address_line_3  varchar(300)    encode  ZSTD,
+    region          varchar(300)    encode  ZSTD,
+    city            varchar(300)    encode  ZSTD,
+    postcode        int             encode  AZ64,
+    paying_flag     bool            encode  ZSTD
+)
+diststyle       key
+distkey         (cust_id)   
+sortkey         (row_id,startdateutc)
+;
